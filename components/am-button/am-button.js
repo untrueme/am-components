b345aa974688b42644c5764825a114334f6d03a4
Азат Ямалов <yamalov@bars.group>
2021-06-12 20:47:23 +0300

    Hover у кнопки

--- components/am-button/am-button.js
+++ components/am-button/am-button.js
@@ -1,66 +1,69 @@
-import { html, css } from 'lit';
-import { SlcElement} from "../slc-element/slc-element.js";
-
-export class AmButton extends SlcElement {
+import { html, css, LitElement } from 'lit';
+export class AmButton extends LitElement {
 
 	static get properties() {
 		return {
-			label: { type: String, value: 'label' },
+			label: { type: String },
 			disabled: { type: Boolean, reflect: true }
 		}
 	}
+
 	static get styles() {
 		return css`
-          :host {
-			display: inline-flex;
-			align-items: center;
-			justify-content: center;
-			position: relative;
-			box-sizing: border-box;
-			background: transparent;
-			outline-width: 0;
-			cursor: pointer;
-			font-weight: normal;
-			white-space: nowrap;
-			min-width: fit-content;
-			line-height: 32px;
-			padding: 12px 12px;
-			height: 32px;
-			background: #209CEE;
-			color: white;
-		}
+			:host {
+				display: inline-flex;
+				align-items: center;
+				justify-content: center;
+				position: relative;
+				box-sizing: border-box;
+				background: transparent;
+				outline-width: 0;
+				cursor: pointer;
+				font-weight: normal;
+				white-space: nowrap;
+				min-width: fit-content;
+				line-height: 32px;
+				padding: 12px 12px;
+				height: 32px;
+				background: #209CEE;
+				color: white;
+			}
 
-		:host([hidden]) {
-			display: none !important;
-		}
+			:host([hidden]) {
+				display: none !important;
+			}
 
-		:host([disabled]) {
-			background: #f5f7f8;
-			color: lightgray;
-			cursor: default;
-			pointer-events: none;
-			user-select: none;
-		}
+			:host([disabled]) {
+				background: #f5f7f8;
+				color: lightgray;
+				cursor: default;
+				pointer-events: none;
+				user-select: none;
+			}
 
-		:host .prefix {
-			display: flex;
-		}
+			:host(:hover) {
+				background: #1c273d
+			}
 
-		:host .prefix ::slotted(*) {
-			margin-right: 8px;
-			width: 16px;
-			height: 16px;
-		}
+			:host .prefix {
+				display: flex;
+			}
 
-		:host .suffix {
-			display: flex;
-		}
+			:host .prefix ::slotted(*) {
+				margin-right: 8px;
+				width: 16px;
+				height: 16px;
+			}
 
-		:host .suffix ::slotted(*) {
-			margin-left: 8px;
-			width: 16px;
-			height: 16px;
-		}
+			:host .suffix {
+				display: flex;
+			}
+
+			:host .suffix ::slotted(*) {
+				margin-left: 8px;
+				width: 16px;
+				height: 16px;
+			}
 		`;
 	}
 	render() {
