85e50915554764146700056d3cc72279d68daf25
Азат Ямалов <yamalov@bars.group>
2021-06-21 08:09:14 +0300

    Правка раскрытия грида

--- components/am-grid/am-grid-row.js
+++ components/am-grid/am-grid-row.js
@@ -50,6 +50,7 @@ class AmGridRow extends LitElement {
                 display: flex;
                 min-height: fit-content;
                 padding: 0px 8px;
+                box-sizing: border-box;
                 overflow: hidden;
                 -webkit-animation: slide-bottom 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
 	            animation: slide-bottom 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
