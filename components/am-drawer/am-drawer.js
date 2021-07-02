784c02906d23496a0bf0801309b336592f3c8ae5
Азат Ямалов <yamalov@bars.group>
2021-07-02 17:33:49 +0300

    drawer

--- components/am-drawer/am-drawer.js
+++ components/am-drawer/am-drawer.js
@@ -17,11 +17,13 @@ class AmDrawer extends LitElement {
                 padding: 16px;
                 width: 300px;
                 height: 100%;
-                position: absolute;
-                background: red;
+                position: fixed;
+                background: #fff;
+                box-shadow: var(--nv-box-shadow);
                 top: 0px;
+                will-change: contents;
                 left: -130%;
-                z-index: 3;
+                z-index: 999;
                 transition: left .3s cubic-bezier(0.820, 0.085, 0.395, 0.895);
             }
 
@@ -34,7 +36,12 @@ class AmDrawer extends LitElement {
     render() {
         return html`
             <slot></slot>
+            <am-button label="close" @click="${this.close}"></am-button>
 		`;
+    }
+
+    close(){
+        this.dispatchEvent(new CustomEvent('close'));
     }
 }
 
